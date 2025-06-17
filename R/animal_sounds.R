7ad95549934707b9ac0d8dc5c6a278791e2b495e
ElisDi <vilde.dietz@ndm.ox.ac.uk>
2025-06-17 16:08:34 +0100

    Update description, and add help page

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,4 +1,17 @@
 
+#' Retrieve animal sounds
+#'
+#' This function takes an animal and a sound as inputs and returns a string
+#' @param animal A character string representing the animal
+#' @param sound A character string representing the sound the animal makes
+#'
+#' @returns A character string in the format "The [animal] says [sound]!"
+#' @export
+#'
+#' @examples
+#' animal_sounds("dog", "woof")
+#' animal_sounds("cat", "meow")
+
 animal_sounds <- function(animal, sound) {
   stopifnot(is.character(animal) & length(animal) == 1)
   stopifnot(is.character(sound) & length(sound) == 1)
